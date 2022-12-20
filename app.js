36d01157fa1fefda1e47762c71e9fefbdff0fef6
julian <juka1031@gmail.com>
2022-12-20 08:25:20 -0500

    changed mongo_uri dynamic

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 mongoose
-  .connect(process.env.MONGO_UR, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
